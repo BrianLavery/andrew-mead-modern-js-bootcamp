c74fabd865d9d82a9e8d68e82b521f6cc11a22c6
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-08-16 12:50:10 +0800

    Completed to lecture 11

--- basics/002-strings.js
+++ basics/002-strings.js
@@ -1,6 +1,12 @@
 let firstName = 'Brian';
 let lastName = 'Lavery';
-
 let fullName = firstName + ' ' + lastName;
 
 console.log(fullName)
+
+
+let city = 'Portadown';
+let country = 'Ireland';
+let location = city + ', ' + country;
+
+console.log(location)
---
+++ basics/003-numbers.js
@@ -0,0 +1,15 @@
+let num = 5.5
+
+console.log(num)
+
+num = 5.5 - 1 
+
+console.log(num)
+
+
+// Challenge
+
+let studentScore = 18
+let maxScore = 20
+let percent = studentScore / maxScore * 100
+console.log(percent)
