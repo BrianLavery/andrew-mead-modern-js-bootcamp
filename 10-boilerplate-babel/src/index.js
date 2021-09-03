44cb1d34c09f7c0d931e3e9e73cb30ad7ee6e430
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-09-03 23:44:30 +0700

    up to lecture 132

---
+++ 10-boilerplate-babel/src/index.js
@@ -0,0 +1,11 @@
+const personName = 'Mike'
+console.log(personName)
+
+class Hangman {
+  myMethod() {
+    return 'Testing'
+  }
+}
+
+const hangman = new Hangman()
+console.log(hangman.myMethod())
